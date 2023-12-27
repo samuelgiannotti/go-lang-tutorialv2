f5015d91a1f5eb644d555b26b275a91a63de9410
Samuel Giannotti <samuelgiannotti@hotmail.com>
2023-12-27 18:36:00 -0300

    call back implementation of azure service bus queue listener, that reads a messagem from queue and insert a record in azure sql server

--- MessagerListenerV2/go.mod
+++ MessagerListenerV2/go.mod
@@ -1,6 +1,6 @@
 module messagelistenerV2
 
-go 1.20
+go 1.21
 
 require (
 	github.com/Azure/azure-sdk-for-go/sdk/azcore v1.4.0 // indirect
@@ -8,12 +8,15 @@ require (
 	github.com/Azure/azure-sdk-for-go/sdk/internal v1.2.0 // indirect
 	github.com/Azure/azure-sdk-for-go/sdk/messaging/azservicebus v1.2.1 // indirect
 	github.com/AzureAD/microsoft-authentication-library-for-go v0.9.0 // indirect
+	github.com/denisenkom/go-mssqldb v0.12.3 // indirect
 	github.com/golang-jwt/jwt/v4 v4.5.0 // indirect
+	github.com/golang-sql/civil v0.0.0-20220223132316-b832511892a9 // indirect
+	github.com/golang-sql/sqlexp v0.1.0 // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/kylelemons/godebug v1.1.0 // indirect
 	github.com/pkg/browser v0.0.0-20210911075715-681adbf594b8 // indirect
-	golang.org/x/crypto v0.6.0 // indirect
-	golang.org/x/net v0.7.0 // indirect
-	golang.org/x/sys v0.5.0 // indirect
-	golang.org/x/text v0.7.0 // indirect
+	golang.org/x/crypto v0.17.0 // indirect
+	golang.org/x/net v0.10.0 // indirect
+	golang.org/x/sys v0.15.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
 )
--- MessagerListenerV2/main.go
+++ MessagerListenerV2/main.go
@@ -2,76 +2,122 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	"fmt"
+	"os"
+	"strings"
 
-	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
 	"github.com/Azure/azure-sdk-for-go/sdk/messaging/azservicebus"
+
+	_ "github.com/denisenkom/go-mssqldb"
 )
 
 func GetClient() *azservicebus.Client {
-	//namespace, ok := os.LookupEnv("AZURE_SERVICEBUS_HOSTNAME") //ex: myservicebus.servicebus.windows.net
-	//if !ok {
-	//	panic("AZURE_SERVICEBUS_HOSTNAME environment variable not found")
-	//}
-	namespace := "viverorganizer.servicebus.windows.net"
-
-	cred, err := azidentity.NewDefaultAzureCredential(nil)
-	if err != nil {
-		panic(err)
+	namespace, ok := os.LookupEnv("SBCONNSTR")
+	if !ok {
+		panic("SBCONNSTR environment variable not found")
 	}
-
-	client, err := azservicebus.NewClient(namespace, cred, nil)
+	client, err := azservicebus.NewClientFromConnectionString(namespace, nil)
 	if err != nil {
 		panic(err)
 	}
 	return client
 }
 
-func SendMessage(message string, client *azservicebus.Client) {
-	sender, err := client.NewSender("nop.to.mailmarketing.qa", nil)
-	if err != nil {
-		panic(err)
+func insertMktLead(name string, email string) {
+	mailMktConnStr, ok := os.LookupEnv("MKTDBCONNSTR")
+	if !ok {
+		panic("MKTDBCONNSTR environment variable not found")
 	}
-	defer sender.Close(context.TODO())
+	Mktconn, err := sql.Open("mssql", mailMktConnStr)
 
-	sbMessage := &azservicebus.Message{
-		Body: []byte(message),
-	}
-	err = sender.SendMessage(context.TODO(), sbMessage, nil)
 	if err != nil {
 		panic(err)
+	}
+
+	ctx := context.Background()
+	err = Mktconn.PingContext(ctx)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	tsql := fmt.Sprintf("SELECT [Name] FROM [EmailMarketing].[MailList] where [email] = '" + email + "';")
+
+	// Execute query
+	rows, err := Mktconn.QueryContext(ctx, tsql)
+	if err != nil {
+		fmt.Println("Erro ao pesquisar lead:", err)
+	}
+
+	defer rows.Close()
+
+	// Iterate through the result set.
+	if rows.Next() {
+		var name string
+		err := rows.Scan(&name)
+		if err != nil {
+			fmt.Println("Erro ao carregar o lead", err)
+		}
+		fmt.Println("Lead:", name)
+	} else {
+		//ctx := context.Background()
+		fmt.Println("Lead not found! ", email)
+		tsql := "INSERT INTO [EmailMarketing].[MailList] ([Name], [email]) VALUES ('" + name + "', '" + email + "'); select isNull(SCOPE_IDENTITY(), -1);"
+
+		stmt, err := Mktconn.Prepare(tsql)
+		if err != nil {
+			fmt.Println("Erro ao conectar para inserir lead:", err)
+			return
+		}
+		defer stmt.Close()
+
+		row := stmt.QueryRowContext(
+			ctx)
+		var newID int64
+		err = row.Scan(&newID)
+		if err != nil {
+			fmt.Println("Erro ao inserir lead:", err)
+			return
+		}
+		fmt.Printf("novo lead inserido: nome: %s id:%d", name, newID)
 	}
 }
 
 func GetMessage(count int, client *azservicebus.Client) {
-	receiver, err := client.NewReceiverForQueue("myqueue", nil)
+	queueName, ok := os.LookupEnv("QUEUENAME")
+	if !ok {
+		panic("QUEUENAME environment variable not found")
+	}
+	receiver, err := client.NewReceiverForQueue(queueName, nil)
 	if err != nil {
 		panic(err)
 	}
 	defer receiver.Close(context.TODO())
 
-	messages, err := receiver.ReceiveMessages(context.TODO(), count, nil)
-	if err != nil {
-		panic(err)
-	}
-
-	for _, message := range messages {
-		body := message.Body
-		fmt.Printf("%s\n", string(body))
-
-		err = receiver.CompleteMessage(context.TODO(), message, nil)
+	for {
+		messages, err := receiver.ReceiveMessages(context.TODO(), count, nil)
 		if err != nil {
-			panic(err)
+			fmt.Println("Erro ao receber mensagem:", err)
+			continue
 		}
+
+		for _, message := range messages {
+			body := message.Body
+			fmt.Printf("%s\n", string(body))
+			customer := strings.Split(string(body), ";")
+			insertMktLead(customer[0], customer[1])
+			err = receiver.CompleteMessage(context.TODO(), message, nil)
+			if err != nil {
+				panic(err)
+			}
+		}
+
 	}
 }
 
 func main() {
 	client := GetClient()
 
-	fmt.Println("send a single message...")
-	SendMessage("firstMessage", client)
-
-	fmt.Println("\nget all three messages:")
-	GetMessage(3, client)
+	fmt.Println("ms nop to mail marketing - geting messages")
+	GetMessage(1, client)
 }
